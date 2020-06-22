1213d21ce8f914e3e74379122867eaaa322020ca
Jordan <jordan@figgy.dev>
2020-06-22 02:11:45 +0000

    Releasing new version

--- Formula/figgy.rb
+++ Formula/figgy.rb
@@ -6,7 +6,7 @@ class Figgy < Formula
     
     bottle do
         root_url "https://www.figgy.dev"
-        rebuild "1"
+        rebuild 1
         sha256 "81c60a74d991af1cd8e8063d45d3ec2ca0c18b8631638f86ada994af2addfe26" => :catalina
         sha256 "81c60a74d991af1cd8e8063d45d3ec2ca0c18b8631638f86ada994af2addfe26" => :mojave
         sha256 "81c60a74d991af1cd8e8063d45d3ec2ca0c18b8631638f86ada994af2addfe26" => :high_sierra

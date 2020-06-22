f2c6e175d1e89eb002132f54000b7e0cd4da473a
Jordan Mance <mancej@snagajob.com>
2020-06-22 09:23:37 -0400

    preventing auto deploy in all situations

--- update_formula.py
+++ update_formula.py
@@ -46,15 +46,14 @@ print(f'Got SHA256 for {dest}: {sha256}')
 for arch in architectures:
     create_bottle(version, arch, BOTTLE_VERSION)
 
-# if pypi_version != current_version:
-print(f"Updating figgy.rb to latest version: {version} with url: {download_url}")
-contents = BREW_TEMPLATE\
-    .replace('%%URL%%', download_url)\
-    .replace('%%SHA%%', sha256)\
-    .replace('%%BOTTLE_VERSION%%', str(BOTTLE_VERSION))
+if pypi_version != current_version:
+    print(f"Updating figgy.rb to latest version: {version} with url: {download_url}")
+    contents = BREW_TEMPLATE\
+        .replace('%%URL%%', download_url)\
+        .replace('%%SHA%%', sha256)\
+        .replace('%%BOTTLE_VERSION%%', str(BOTTLE_VERSION))
 
-with open('Formula/figgy.rb', 'w+') as file:
-    file.write(contents)
-#
-# else:
-#     print("Homebrew version is the same as latest version for figgy. Not updating.")
+    with open('Formula/figgy.rb', 'w+') as file:
+        file.write(contents)
+else:
+    print("Homebrew version is the same as latest version for figgy. Not updating.")

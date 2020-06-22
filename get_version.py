cbde1fb251de38f500f71677b53ffb9cf6cfb78e
Jordan Mance <mancej@snagajob.com>
2020-06-22 17:15:08 -0400

    Fixing version parsing

--- get_version.py
+++ get_version.py
@@ -2,15 +2,18 @@ import re
 import requests
 
 result = requests.get('https://pypi.org/project/figgy-cli/#files')
+
 if result.status_code != 200:
     raise ValueError("Invalid status code returned from PYPI, cannot lookup latest version.")
 
 data = result.text
-version_match = re.search(r'.*release__version\">\s*([0-9]+\.[0-9]+\.[0-9]+\w*)\s+<span.*', data, re.MULTILINE)
+tar_match = re.search(r'.*href=\"(https://files.pythonhosted.org/.*figgy-cli-[0-9]+\.[0-9]+\.[0-9]+\w*\.tar\.gz)\".*',
+                      data, re.MULTILINE)
+download_url = tar_match.group(1)
+
+version_match = re.search(r'.*figgy-cli-([0-9]+\.[0-9]+\.[0-9]+\w*)\.tar\.gz', download_url)
 pypi_version = version_match.group(1)
 
-# print(pypi_version)
 
 # Remove suffix integer.
-figgy_version = pypi_version.rstrip('0123456789')
-print(figgy_version)
+print(pypi_version)

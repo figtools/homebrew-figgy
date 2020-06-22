4fec86093312b6d908d243b6c258b74fc059ebf7
Jordan <jordan@figgy.dev>
2020-06-22 02:21:12 +0000

    Releasing new version

--- Formula/figgy.rb
+++ Formula/figgy.rb
@@ -1,7 +1,7 @@
 class Figgy < Formula
     desc "This is the CLI that accompanies the `figgy` configuration management framework."
     homepage "https://figgy.dev"
-    url "https://files.pythonhosted.org/packages/89/2e/13a43c79a8b0561185d42176b4798f8ebc8fc497e01c076e2a4eb282a4fc/figgy-cli-0.0.30a0.tar.gz"
+    url "https://www.figgy.dev/releases/cli/0.0.30a/darwin/figgy.tar.gz"
     sha256 "81c60a74d991af1cd8e8063d45d3ec2ca0c18b8631638f86ada994af2addfe26"
     
     bottle do

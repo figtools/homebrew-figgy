ce4468965ec24bfd651c66a1fecd9b5d4ad31d7e
Jordan Mance <mancej@snagajob.com>
2020-06-25 08:27:29 -0400

    Testing repo structure to support old versions

--- Formula/figgy@0.0.33.rb
+++ Formula/figgy@0.0.33.rb
@@ -1,4 +1,4 @@
-class FiggyAT0.0.33 < Formula
+class FiggyAT0033 < Formula
     desc "This is the CLI that accompanies the `figgy` configuration management framework."
     homepage "https://figgy.dev"
     url "https://www.figgy.dev/releases/cli/0.0.33/darwin/figgy.tar.gz"

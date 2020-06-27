610d1da6eb959387abfe57063882098cbe0ee43e
Jordan <jordan@figgy.dev>
2020-06-27 20:46:26 +0000

    Releasing new version

--- Formula/figgy.rb
+++ Formula/figgy.rb
@@ -1,15 +1,15 @@
 class Figgy < Formula
     desc "This is the CLI that accompanies the `figgy` configuration management framework."
     homepage "https://figgy.dev"
-    url "https://www.figgy.dev/releases/cli/0.0.44/darwin/figgy.tar.gz"
-    sha256 "252f448dfd980b20178793a87a1ea05527662d6e00f484e683a863634c31143b"
+    url "https://www.figgy.dev/releases/cli/0.0.45/darwin/figgy.tar.gz"
+    sha256 "125e60a7ba51551baedacd88d8590ffad6384ea006dbf3efd27733570d0f7773"
     
     bottle do
         root_url "https://www.figgy.dev/bottles"
         rebuild 1
-        sha256 "252f448dfd980b20178793a87a1ea05527662d6e00f484e683a863634c31143b" => :catalina
-        sha256 "252f448dfd980b20178793a87a1ea05527662d6e00f484e683a863634c31143b" => :mojave
-        sha256 "252f448dfd980b20178793a87a1ea05527662d6e00f484e683a863634c31143b" => :high_sierra
+        sha256 "125e60a7ba51551baedacd88d8590ffad6384ea006dbf3efd27733570d0f7773" => :catalina
+        sha256 "125e60a7ba51551baedacd88d8590ffad6384ea006dbf3efd27733570d0f7773" => :mojave
+        sha256 "125e60a7ba51551baedacd88d8590ffad6384ea006dbf3efd27733570d0f7773" => :high_sierra
     end
 
   # depends_on "cmake" => :build
---
+++ Formula/figgy@0.0.44.rb
@@ -0,0 +1,24 @@
+class FiggyAT0044 < Formula
+    desc "This is the CLI that accompanies the `figgy` configuration management framework."
+    homepage "https://figgy.dev"
+    url "https://www.figgy.dev/releases/cli/0.0.44/darwin/figgy.tar.gz"
+    sha256 "252f448dfd980b20178793a87a1ea05527662d6e00f484e683a863634c31143b"
+    
+    bottle do
+        root_url "https://www.figgy.dev/bottles"
+        rebuild 1
+        sha256 "252f448dfd980b20178793a87a1ea05527662d6e00f484e683a863634c31143b" => :catalina
+        sha256 "252f448dfd980b20178793a87a1ea05527662d6e00f484e683a863634c31143b" => :mojave
+        sha256 "252f448dfd980b20178793a87a1ea05527662d6e00f484e683a863634c31143b" => :high_sierra
+    end
+
+  # depends_on "cmake" => :build
+
+    def install
+        puts "installing"
+    end
+    
+    test do
+        system "#{bin}/figgy", "--version"
+    end
+end

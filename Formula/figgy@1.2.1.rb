321f0212f001fa0d1dbb927e232de37de1c37023
Jordan <jordan@figgy.dev>
2021-10-01 01:07:50 +0000

    Releasing new version

--- Formula/figgy@1.2.1.rb
+++ Formula/figgy@1.2.1.rb
@@ -1,14 +1,14 @@
-class Figgy < Formula
+class FiggyAT121 < Formula
     desc "This is the CLI that accompanies the `figgy` configuration management framework."
     homepage "https://figgy.dev"
-    url "https://www.figgy.dev/releases/cli/1.2.2/darwin/figgy.tar.gz"
-    sha256 "401cbb438817f8b235ada19cc86d2f9f8c615d47138cede9213fc76244f16b3b"
+    url "https://www.figgy.dev/releases/cli/1.2.1/darwin/figgy.tar.gz"
+    sha256 "0f80c73a5912f9dc0fb4ec95e2d0a6a74756b28e6b918888c703e359b9f7f838"
     
     bottle do
         root_url "https://www.figgy.dev/bottles"
         rebuild 1
-        sha256 "401cbb438817f8b235ada19cc86d2f9f8c615d47138cede9213fc76244f16b3b" => :catalina
-        sha256 "401cbb438817f8b235ada19cc86d2f9f8c615d47138cede9213fc76244f16b3b" => :mojave
+        sha256 "0f80c73a5912f9dc0fb4ec95e2d0a6a74756b28e6b918888c703e359b9f7f838" => :catalina
+        sha256 "0f80c73a5912f9dc0fb4ec95e2d0a6a74756b28e6b918888c703e359b9f7f838" => :mojave
     end
 
   # depends_on "cmake" => :build

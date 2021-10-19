f34378cd00f37ca32851acee14dea56804ab6769
Jordan Mance <mancej@snagajob.com>
2021-10-19 17:32:22 -0400

    Fixing bug with cloud version requirement

--- release.sh
+++ release.sh
@@ -13,4 +13,4 @@ echo "Setting current version cloud requirement to: $cloud_version_requirement"
 
 aws ssm put-parameter --name /figgy/deployments/current_version --type "String" --value "$version" --region 'us-east-1' --overwrite || die "Error updating figgy version in ParameterStore"
 aws ssm put-parameter --name /figgy/deployments/rollout_modifier --type "String" --value "100" --region 'us-east-1' --overwrite || die "Error updating figgy rollout modifier in ParameterStore"
-aws ssm put-parameter --name /figgy/deployments/cloud_version_requirement --type "String" --value "$version" --region 'us-east-1' --overwrite || die "Error updating figgy cloud version requirement in ParameterStore"
+aws ssm put-parameter --name /figgy/deployments/cloud_version_requirement --type "String" --value "$cloud_version_requirement" --region 'us-east-1' --overwrite || die "Error updating figgy cloud version requirement in ParameterStore"
